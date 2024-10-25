4e6be71cbb9365729ac61d8111f0ff6975141e3e
Christophe YOSSA <christophe.yossawandji@monboreal.ca>
2024-10-25 10:52:39 -0400

    Creation classe Utilisateur

---
+++ Utilisateur.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Utilisateur
+    {
+        public string Nom { get; set; }
+        public List<Paiement> Paiements { get; set; }
+        public Utilisateur(string nom)
+        {
+            Nom = nom;
+            Paiements = new List<Paiement>();
+        }
+        public void Afficherinfos()
+
+        {
+            Console.WriteLine($"Nom:{Nom}");
+            foreach (Paiement p in Paiements)
+            {
+                p.AfficherDetails();
+            }
+
+        }
+
+        public void AjouterPaiement(Paiement p)
+        {
+            Paiements.Add(p);
+        }
+    }
+}
+
