cf3a35e7324ef0d173a1ce269208b0b8bcee8a06
Christophe YOSSA <christophe.yossawandji@monboreal.ca>
2024-10-25 10:37:12 -0400

    Creation classe Paypal

---
+++ PayPal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class PayPal : Paiement
+    {
+        public string Courriel { get; set; }
+        public PayPal(double montant, string description, string email) : base(montant, description)
+        {
+            Montant = montant;
+            Description = description;
+            Courriel = email;
+        }
+
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"Montant:{Montant}, Description:{Description}, email:{Courriel}");
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        //CARTE CREDIT
         //Création de trois instances de CarteCredit
         CarteCredit achat1 = new CarteCredit(12.5,"Achat Electroniques ",398);
         CarteCredit achat2 = new CarteCredit(89.50, "Achat Vetements", 677);
@@ -12,6 +13,16 @@ internal class Program
         achat1.AfficherDetails();
         achat2.AfficherDetails();
         achat3.AfficherDetails();
+
+        //PAYPAL
+        //Création de deux instances de PayPal
+        PayPal buy1 = new PayPal(12.5, "Achat Electroniques ", "chrisyos@yahoo.fr");
+        PayPal buy2 = new PayPal(89.50, "Achat Vetements", "300141146@monboreal.ca");
+        
+        //Affichage des détails des achats
+        buy1.AfficherDetails();
+        buy2.AfficherDetails();
+
 
     }
 }
