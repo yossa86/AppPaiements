8ebe59e5fb2b6dfe1cd45bb76947e1704315269b
Christophe YOSSA <christophe.yossawandji@monboreal.ca>
2024-10-25 10:02:25 -0400

    Creation Classe CarteCredit

--- Program.cs
+++ Program.cs
@@ -2,6 +2,15 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Création de trois instances de CarteCredit
+        CarteCredit achat1 = new CarteCredit(12.5,"Achat Electroniques ",398);
+        CarteCredit achat2 = new CarteCredit(89.50, "Achat Vetements", 677);
+        CarteCredit achat3 = new CarteCredit(45.75, "Achat Alimentation", 782);
+
+        //Affichage des détails des achats
+        achat1.AfficherDetails();
+        achat2.AfficherDetails();
+        achat3.AfficherDetails();
+
     }
 }
